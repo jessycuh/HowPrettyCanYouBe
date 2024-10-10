d07e01475d28825ce179ee57f908f4ca3a284168
jessycuh <lalalalaplays@gmail.com>
2024-10-09 21:42:05 -0700

    Update score.js

--- score.js
+++ score.js
@@ -1,17 +1,25 @@
 let score = localStorage.getItem('playerScore');
 
-// Check if we're on the specific page (e.g., 'menu.html' in this case)
 if (window.location.pathname.includes('menu.html')) {
-    // Reset score on this specific page only
-    score = Math.floor(Math.random() * 10) + 1; // Random score between 1-50
-    localStorage.setItem('playerScore', score); // Store the new score
-} else if (score !== null) {
-    // Parse score for other pages
+
+    score = Math.floor(Math.random() * 50) + 1; 
+    localStorage.setItem('playerScore', score); 
+} else if (score === null) {
+
+    score = 0; 
+} else {
+
     score = parseInt(score, 10);
 }
 
-function displayScore() {
-    document.getElementById("playerScore").textContent = score; // Display the score
+if (score > 200) {
+    score = 200; 
 }
+
+
+function displayScore() {
+    document.getElementById("playerScore").textContent = score;
+}
+
 
 window.onload = displayScore;
