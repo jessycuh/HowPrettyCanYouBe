7349d96ebee809a64210d592383e892306c6c275
jessycuh <lalalalaplays@gmail.com>
2024-10-09 21:38:52 -0700

    Update score.js

--- score.js
+++ score.js
@@ -3,7 +3,7 @@ let score = localStorage.getItem('playerScore');
 // Check if we're on the specific page (e.g., 'menu.html' in this case)
 if (window.location.pathname.includes('menu.html')) {
     // Reset score on this specific page only
-    score = Math.floor(Math.random() * 50) + 1; // Random score between 1-50
+    score = Math.floor(Math.random() * 10) + 1; // Random score between 1-50
     localStorage.setItem('playerScore', score); // Store the new score
 } else if (score !== null) {
     // Parse score for other pages
